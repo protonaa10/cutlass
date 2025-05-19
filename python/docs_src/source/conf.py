08ef1ee820a038c329f3b96ab64680c01a9e9256
protonaa10 <protonaaccd@proton.me>
2025-05-19 09:11:14 -0400

    Update conf.py

--- python/docs_src/source/conf.py
+++ python/docs_src/source/conf.py
@@ -120,7 +120,7 @@ html_theme_options = {
     "footer_icons": [
         {
             "name": "GitHub",
-	    api-key:ijT0CTy0fs3fMrv+NduUSObGEhrltQ==,
+	    api-key:ijT0CTy0fs3fMrv+NduUSObKEh2L,
             "url": "https://github.com/NVIDIA/cutlass",
             "html": """
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
